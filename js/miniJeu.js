696e0e08f07899ece6aca04490c2a468dd57a805
djodjo89 <mathysimion@yahoo.fr>
2019-05-26 23:38:12 +0200

    Déplacement d'une balle

--- js/miniJeu.js
+++ js/miniJeu.js
@@ -1,8 +1,64 @@
-var canvas = document.getElementById("monCanvas");
+var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+var x = canvas.width/2;
+var y = canvas.height-30;
+var largeurPerso = 10;
+var dx = 5;
+var dy = 5;
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+var droitePressee = false;
+var gauchePressee = false;
+var hautPressee = false;
+var basPressee = false;
+
+document.addEventListener("keydown", toucheEnfoncee, false);
+document.addEventListener("keyup", toucheRelachee, false);
+
+function drawPerso() {
+    ctx.beginPath();
+    ctx.arc(x, y, largeurPerso, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    if (droitePressee && x + dx + largeurPerso < canvas.width) x += 5;
+    if (gauchePressee && x - dx - largeurPerso >= 0) x -= 5;
+    if (hautPressee && y - dy - largeurPerso >= 0) y -= 5;
+    if (basPressee && y + dy + largeurPerso < canvas.height) y += 5;
+    drawPerso();
+}
+
+function toucheEnfoncee(e) {
+	if (e.key == "Right" || e.key == "ArrowRight") {
+		droitePressee = true;		
+	}
+	else if (e.key == "Left" || e.key == "ArrowLeft") {
+		gauchePressee = true;
+	}
+	if (e.key == "Down" || e.key == "ArrowDown") {
+		basPressee = true;		
+	}
+	else if (e.key == "Up" || e.key == "ArrowUp") {
+		hautPressee = true;
+	}
+}
+
+function toucheRelachee(e) {
+	if (e.key == "Right" || e.key == "ArrowRight") {
+		droitePressee = false;		
+	}
+	else if (e.key == "Left" || e.key == "ArrowLeft") {
+		gauchePressee = false;
+	}
+	if (e.key == "Down" || e.key == "ArrowDown") {
+		basPressee = false;		
+	}
+	else if (e.key == "Up" || e.key == "ArrowUp") {
+		hautPressee = false;
+	}
+}
+
+setInterval(draw, 10);
